de2b8ca2a5a673035395efa39aa7796af889cb0a
Adla <adlangar8@gmail.com>
2026-04-21 17:58:13 +0300

    style: enhance BusinessProfile UI with grid layout and luxury theme

--- front_end/src/components/CommonComponents/BusinessProfile.jsx
+++ front_end/src/components/CommonComponents/BusinessProfile.jsx
@@ -13,7 +13,6 @@ export default function BusinessProfile({user,provider}) {
   useEffect(() => {
     const fetchProfile = async () => {
       console.log("I AM IN BUSINESSPROFILE" + user.role)
-
       try {
         const rolePath=user.role.toLowerCase();
         const url=`http://localhost:3030/${rolePath}/Profile/${provider.id}`
