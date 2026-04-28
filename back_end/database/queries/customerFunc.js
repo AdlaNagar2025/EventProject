0d25c8b7feef8e40f766575ad40999cd920fbd77
AdlaNagar2025 <adlangar8@gmail.com>
2026-04-28 12:50:07 +0300

    feat:initail Searching

--- back_end/database/queries/customerFunc.js
+++ back_end/database/queries/customerFunc.js
@@ -1,25 +1,22 @@
-
 const doQuery = require("../query");
 
-
 async function getResultSearching(dataToSearch) {
-    const city = dataToSearch.city || "";
-    const capacity = dataToSearch.capacity || 0; 
-    const price = dataToSearch.price || 999999; 
+  const city = dataToSearch.city || "";
+  const capacity = dataToSearch.capacity || 0;
+  const price = dataToSearch.price || 999999;
+  const values = [city, capacity, price, city, capacity, price];
+  console.log(city);
+  console.log(capacity);
+  console.log(price);
 
-    const sql = `
-      SELECT city, capacity, price , "halls" as type , halls.hall_id as id  FROM halls 
-WHERE city =? AND capacity >= ? AND price <= ?
-        UNION
-SELECT  city, capacity, price_per_hour as price , "chiefs" as type   , chiefs.chief_id as id FROM chiefs
-WHERE city =? AND capacity >= ? AND price_per_hour <= ?;
-    `;
+  const sql = `select * , role as provider_type FROM users where id in
+ (SELECT  hall_id as id FROM halls WHERE city=? AND capacity >=? AND price<=?
+ UNION 
+SELECT  chiefs.chief_id as id FROM chiefs WHERE city=? AND capacity >=? AND chiefs.price_per_hour<=?
+);`;
+  const result = await doQuery(sql, values);
 
-    const values = [city, capacity, price, city, capacity, price];
-    
-    const result = await doQuery(sql, values);
-    return result;
+  return result;
 }
 
-module.exports={getResultSearching}
-
+module.exports = { getResultSearching };
